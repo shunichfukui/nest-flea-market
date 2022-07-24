566cc80bbb3381fd26edac8e0887e8738f17edfa
shunich <fukusyun3@gmail.com>
2022-07-24 14:26:42 +0900

    feat: 例外処理追加

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 import { ItemStatus } from './item-status.enum';
 import { Item } from './item.model';
@@ -13,7 +13,12 @@ export class ItemsService {
     }
 
     findById(id: string): Item {
-        return this.items.find((item) => item.id === id);
+        const item = this.items.find((item) => item.id === id);
+        if (!item) {
+            throw new NotFoundException();
+        }
+
+        return item;
     }
 
     updateStatus(id: string): Item {
