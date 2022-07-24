5f56f102f95c6d43f978894ef86a3fa7c0660d16
shunich <fukusyun3@gmail.com>
2022-07-24 13:44:34 +0900

    feat: crud機能の実装

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { ItemStatus } from './item-status.enum';
 import { Item } from './item.model';
 
 @Injectable()
 export class ItemsService {
     private items: Item[] = [];
 
-    findAll() {
-        return "this is itemsService"
+    findAll(): Item[] {
+        return this.items;
+    }
+
+    findById(id: string): Item {
+        return this.items.find((item) => item.id === id);
+    }
+
+    updateStatus(id: string): Item {
+        const item = this.findById(id);
+        item.status = ItemStatus.SOLD_OUT;
+        return item;
+    }
+
+    deleteItem(id: string): void {
+        this.items = this.items.filter((item) => item.id !== id);
     }
 
     create(item: Item): Item {
